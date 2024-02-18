bfe437fb773ccc191c09d88f8bde69f91956d174
nalendramuhammad <itsnalendraa@gmail.com>
2024-02-18 12:16:16 +0700

    push

--- components/ProdCard.jsx
+++ components/ProdCard.jsx
@@ -12,7 +12,7 @@ const CarCard = ({
   return (
     <div className='w-80 cursor-pointer mb-10 h-1/2'>
       <Image
-        className='brightness-95'
+        className='brightness-95 h-96'
         width={400}
         height={400}
         src={`/producsPage/${img}`}
@@ -21,6 +21,15 @@ const CarCard = ({
       <h2 className='xl:text-xl text-base font-semibold mt-5'>{name}</h2>
       <p className='text-sm xl:text-base text-gray-600 pb-2'>{description}</p>
       <p className='text-sm xl:text-base text-gray-600 pb-2'>{price}</p>
+      <div
+        className='text-sm xl:text-base text-gray-600 pb-2 flex items-center'
+        href='https://wa.me/6281328732245'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <img src='/wa.png' alt='WhatsApp' width={30} height={30} />{" "}
+        <p>beli produk ini</p>
+      </div>
     </div>
   );
 };
